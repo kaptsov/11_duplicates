f8d0b0fce52c94ef98167f8a9f7c28125064d257
kaptsov <kaptsovaa@gmail.com>
2018-03-01 17:01:33 +0900

    Исправлены имена переменных, изменен  вывод

--- duplicates.py
+++ duplicates.py
@@ -11,19 +11,19 @@ def get_directory_path():
 
 def find_duplicates(root_directory):
     dup_files = defaultdict(list)
-    for dirpath, dirnames, files in os.walk(root_directory):
+    for folder, subfolders, files in os.walk(root_directory):
         for filename in files:
-            full_file_path = os.path.join(dirpath, filename)
+            full_file_path = os.path.join(folder, filename)
             file_size = os.stat(full_file_path).st_size
             dup_files[(filename, file_size)].append(full_file_path)
-    return {key[0]: val for key, val in dup_files.items() if len(val) > 1}
+    return {name_size[0]: filelist for name_size, filelist in dup_files.items()
+            if len(filelist) > 1}
 
 
 def print_list(duplicate_list):
-    for key, value in duplicate_list.items():
-        print('File "{0}" found in following directories:'.format(key))
-        for v in value:
-            print('    {0}'.format(v))
+    for name_size, paths in duplicate_list.items():
+        print('File "{0}" found in following directories:'.format(name_size))
+        print('\n\t'.join(paths))
 
 
 if __name__ == '__main__':
